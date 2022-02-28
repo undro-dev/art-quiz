b8f4a86048873469a9d1006d276541dca7cfc2bd
Vitaly Undro <vitalyundro@gmail.com>
2022-03-01 00:09:18 +0300

    create branch gh-pages with project

--- art-quiz/index.js
+++
@@ -1,9 +0,0 @@
-import "./styles/index.scss";
-import router from "./js/router/index.routes";
-
-router(window.location.hash + "#/");
-window.addEventListener("load", () => {
-  window.addEventListener("hashchange", () => {
-    router(window.location.hash);
-  });
-});
